b088f49e319bdca4c79540288f40798da3ea94d2
MinorSc <misolano1192@gmail.com>
2024-07-02 20:00:45 -0600

    Feat Variables

---
+++ JS/Variables.js
@@ -0,0 +1,23 @@
+//lenguaje de programación 
+
+// Javascript no es lo mismo que Java
+
+
+//Mostrar mensaje en la consola del navegador
+console.log('Hola Mundo');
+
+// 1. Las variables almacenar datos var = let
+// const PI = 3,14
+
+
+let nombre = "Minor"
+let apellido = "Solano"
+let edad = "31"
+let dirección = "Cartago"
+
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(dirección)
+
