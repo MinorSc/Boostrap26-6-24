385b07cd2b9e4f1491f796e17f83ac1ae0d7265d
MinorSc <misolano1192@gmail.com>
2024-07-02 21:26:38 -0600

    Feat Variables explicacion

--- JS/Variables.js
+++ JS/Variables.js
@@ -3,19 +3,31 @@
 // Javascript no es lo mismo que Java
 
 
-//Mostrar mensaje en la consola del navegador
-console.log('Hola Mundo');
+
 
 // 1. Las variables almacenar datos var = let
 // const PI = 3,14
 
 
+
+//Zona para declarar las variables 
 let nombre = "Minor"
 let apellido = "Solano"
 let edad = 34
 let dirección = "Cartago"
 
+let mensaje = "Hola mi nombre es " + nombre + " y mi apellido es " + apellido + " mi edad es " + edad + " vivo en " + dirección;
+let mensaje2 = "Es usted mayor de edad?"
+let ventana;
 
-console.log("Hola mi nombre es "+nombre+" y mi apellido es "+apellido+" mi edad es "+edad+" vivo en "+dirección)
 
 
+
+
+
+//Mostrar mensaje en la consola del navegador
+console.log(mensaje);
+//capturar lo que diga el usuario
+ventana = prompt(mensaje2);
+//mostrar un msj en una ventana emergente
+alert(ventana);
