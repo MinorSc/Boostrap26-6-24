bd14c294af2bd264c12bd69b6157e4bf7fdb9050
MinorSc <misolano1192@gmail.com>
2024-07-11 20:07:35 -0600

    Feat Operaciones

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -1,25 +1,53 @@
 console.log("Funcionando")
 
-function Sumar(event) {
-        event.preventDefault();
-        const Num1 = document.getElementById("Num1");
-        const Num2 = document.getElementById("Num2");
-        const Resultado = document.getElementById("Resultado");
-
-         //parseFloat de string a numero con decimales 
-         // parseInt de string a numero enteros    
-        let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
-        Resultado.textContent = `El resultado es ${calculo}`;
-}
-
-function Restar(event) {
+function Calculadora2(event) {
     event.preventDefault();
-    const Num1 = document.getElementById("Num1");
-    const Num2 = document.getElementById("Num2");
     const Resultado = document.getElementById("Resultado");
+    const Num1 = parseFloat(document.getElementById("Num1").value);
+    const Num2 = parseFloat(document.getElementById("Num2").value);
+    const Operaciones = document.getElementById("Operaciones").value;
+    let calculo;
+    switch (Operaciones) {
+        case "+":
+            calculo = Num1 + Num2
+            break;
+        case "-":
+            calculo = Num1 - Num2
+            break;
+        case "*":
+            calculo = Num1 * Num2
+            break;
+        case "/":
+            calculo = Num1 / Num2
+            break;
 
-     //parseFloat de string a numero con decimales 
-     // parseInt de string a numero enteros    
-    let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
-    Resultado.textContent = `El resultado es ${calculo}`;
+        default:
+            calculo = "Operacion no valida"
+            break;
+    }
+
+    Resultado.textContent = `El resultado es: ${calculo}`
+
+    console.log(Operaciones);
+
 }
+// function Sumar(event) {
+//     event.preventDefault();
+
+
+//     //parseFloat de string a numero con decimales
+//     // parseInt de string a numero enteros
+//     let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
+//     Resultado.textContent = `El resultado es ${calculo}`;
+// }
+
+// function Restar(event) {
+//     event.preventDefault();
+
+
+//     //parseFloat de string a numero con decimales
+//     // parseInt de string a numero enteros
+//     let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
+//     Resultado.textContent = `El resultado es ${calculo}`;
+// }
+
