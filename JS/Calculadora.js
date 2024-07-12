930011343df169b6fcdf49e069dc86635f6239e5
MinorSc <misolano1192@gmail.com>
2024-07-11 19:33:18 -0600

    Feat Calculadora resta

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -1,1 +1,25 @@
 console.log("Funcionando")
+
+function Sumar(event) {
+        event.preventDefault();
+        const Num1 = document.getElementById("Num1");
+        const Num2 = document.getElementById("Num2");
+        const Resultado = document.getElementById("Resultado");
+
+         //parseFloat de string a numero con decimales 
+         // parseInt de string a numero enteros    
+        let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
+        Resultado.textContent = `El resultado es ${calculo}`;
+}
+
+function Restar(event) {
+    event.preventDefault();
+    const Num1 = document.getElementById("Num1");
+    const Num2 = document.getElementById("Num2");
+    const Resultado = document.getElementById("Resultado");
+
+     //parseFloat de string a numero con decimales 
+     // parseInt de string a numero enteros    
+    let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
+    Resultado.textContent = `El resultado es ${calculo}`;
+}
